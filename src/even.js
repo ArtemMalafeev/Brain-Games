68d24245bf4e0bd463361f9dc18e16ebba0a3e9d
ArtemMalafeev <ma.evgenyevich@gmail.com>
2021-05-08 18:43:57 +0300

    change even.js

--- src/even.js
+++ src/even.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const isEven = (number) => number % 2 === 0;
 
-const getRandomInt = () => Math.floor(Math.random() * 1000);
+const getRandomInt = (max) => Math.floor(Math.random() * max);
 
 const playGame = () => {
   console.log('Welcome to the Brain Games!');
@@ -12,10 +12,11 @@ const playGame = () => {
   const regulations = 'Answer "yes" if the number is even, otherwise answer "no".';
   console.log(regulations);
 
+  const MAX_NUMBER = 1000;
   let roundCount = 3;
 
   while (roundCount > 0) {
-    const question = getRandomInt();
+    const question = getRandomInt(MAX_NUMBER);
     const correctAnswer = isEven(question) ? 'yes' : 'no';
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
